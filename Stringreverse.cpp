b5b0f6e2ff64fd055aa3a804dbcd7d3b6eddfca1
pgupta121202 <91672523+pgupta121202@users.noreply.github.com>
2022-09-18 20:02:42 +0530

    Create Stringreverse.cpp

---
+++ Stringreverse.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string str1;
+    cin>>str1;
+    for(int i=str1.length()-1;i>=0;i--){
+        cout<<str1[i];
+    }
+
+    return 0;
+}
