4e1e5f2a3b9ae7a2d699a844543ef76439e30341
pgupta121202 <91672523+pgupta121202@users.noreply.github.com>
2022-12-15 01:09:34 +0530

    Create no.of1"sin binary number.cpp

---
+++ no.of1"sin binary number.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int count=0;
+    while(n!=0){
+        int rem = n%2;
+        if(rem==1){
+            count++;
+        }
+        n=n/2;
+    }
+    
+    
+    cout<<count<<endl;
+
+    return 0;
+}
